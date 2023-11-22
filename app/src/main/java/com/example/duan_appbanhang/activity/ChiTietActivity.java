a0e238dc069f04379f293a0f1613082f3c7b1cdc
PhuongDubai <cokimson900@gmail.com>
2023-11-22 12:02:11 +0700

    đưa dữ liệu vào màn hình chi tiết sản phẩm

--- app/src/main/java/com/example/duan_appbanhang/activity/ChiTietActivity.java
+++ app/src/main/java/com/example/duan_appbanhang/activity/ChiTietActivity.java
@@ -1,16 +1,72 @@
 package com.example.duan_appbanhang.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.example.duan_appbanhang.R;
+import com.example.duan_appbanhang.mode.SanPhamMoi;
+
+import java.text.DecimalFormat;
 
 public class ChiTietActivity extends AppCompatActivity {
-
+    TextView tensp, giasp, mota;
+    Button btnThem;
+    ImageView imghinhanh;
+    Spinner spinner;
+    Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chi_tiet);
+        initView();
+        ActionToolBar();
+        initData();
     }
+
+    private void initData() {
+        SanPhamMoi sanPhamMoi = (SanPhamMoi) getIntent().getSerializableExtra("chitiet");
+        tensp.setText(sanPhamMoi.getTensp());
+        mota.setText(sanPhamMoi.getMota());
+        Glide.with(getApplicationContext()).load(sanPhamMoi.getHinhanh()).into(imghinhanh);
+        DecimalFormat decimalFormat = new DecimalFormat("###,###,###");
+        giasp.setText("Giá: " + decimalFormat.format(Double.parseDouble(sanPhamMoi.getGiasp())) + " VND");
+        Integer[] so = new Integer[]{1,2,3,4,5,6,7,8,9,10};
+        ArrayAdapter<Integer> adapterspin = new ArrayAdapter<>(this, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item);
+        spinner.setAdapter(adapterspin);
+    }
+
+
+    private void initView() {
+        tensp = findViewById(R.id.txttensp);
+        giasp = findViewById(R.id.txtgiasp);
+        mota = findViewById(R.id.txtmotachitiet);
+        btnThem = findViewById(R.id.btnthemvaogiohang);
+        spinner = findViewById(R.id.spinner);
+        imghinhanh = findViewById(R.id.imgchitiet);
+        toolbar = findViewById(R.id.toobbar);
+
+
+    }
+    private void ActionToolBar() {
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+    }
+
 }
--- app/src/main/java/com/example/duan_appbanhang/adapter/SanPhamMoiAdapter.java
+++ app/src/main/java/com/example/duan_appbanhang/adapter/SanPhamMoiAdapter.java
@@ -1,6 +1,7 @@
 package com.example.duan_appbanhang.adapter;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,7 +12,9 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
+import com.example.duan_appbanhang.Interface.ItemClickListener;
 import com.example.duan_appbanhang.R;
+import com.example.duan_appbanhang.activity.ChiTietActivity;
 import com.example.duan_appbanhang.mode.SanPhamMoi;
 
 import java.text.DecimalFormat;
@@ -41,6 +44,19 @@ public class SanPhamMoiAdapter extends RecyclerView.Adapter<SanPhamMoiAdapter.My
         DecimalFormat decimalFormat = new DecimalFormat("###,###,###");
         holder.txtgia.setText("Giá: "+decimalFormat.format(Double.parseDouble(sanPhamMoi.getGiasp())) +" VND");
         Glide.with(context).load(sanPhamMoi.getHinhanh()).into(holder.imghinhanh);
+        holder.setItemClickListener(new ItemClickListener() {
+            @Override
+            public void onClick(View view, int pos, boolean isLongClick) {
+                if (!isLongClick){
+                    // click
+                    Intent intent = new Intent(context, ChiTietActivity.class);
+                    intent.putExtra("chitiet",sanPhamMoi);
+                    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                    context.startActivity(intent);
+
+                }
+            }
+        });
     }
 
     @Override
@@ -49,9 +65,10 @@ public class SanPhamMoiAdapter extends RecyclerView.Adapter<SanPhamMoiAdapter.My
     }
 
 
-    public class MyViewHolder extends RecyclerView.ViewHolder {
+    public class MyViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
         TextView txtgia, txtten;
         ImageView imghinhanh;
+        private ItemClickListener itemClickListener;
 
 
         public MyViewHolder(@NonNull View itemView) {
@@ -59,7 +76,17 @@ public class SanPhamMoiAdapter extends RecyclerView.Adapter<SanPhamMoiAdapter.My
             txtgia = itemView.findViewById(R.id.itemsp_gia);
             txtten = itemView.findViewById(R.id.itemsp_ten);
             imghinhanh = itemView.findViewById(R.id.itemsp_image);
+            itemView.setOnClickListener(this);
 
+        }
+
+        public void setItemClickListener(ItemClickListener itemClickListener) {
+            this.itemClickListener = itemClickListener;
+        }
+
+        @Override
+        public void onClick(View view) {
+            itemClickListener.onClick(view,getAdapterPosition(),false);
         }
     }
 }
