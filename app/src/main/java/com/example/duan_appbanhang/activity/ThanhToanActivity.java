3c3b912346b8478ca2368bdd59c80892f295e03b
PTCuong33317 <149935739+PTCuong33317@users.noreply.github.com>
2023-11-23 14:54:57 +0700

    Thiết kế giao diện đặt hàng

---
+++ app/src/main/java/com/example/duan_appbanhang/activity/ThanhToanActivity.java
@@ -0,0 +1,16 @@
+package com.example.duan_appbanhang.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.duan_appbanhang.R;
+
+public class ThanhToanActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_thanh_toan);
+    }
+}
